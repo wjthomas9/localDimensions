aa39886836572a0ac13277f16238d36d6a6f5d7b
Jon Thomas <jon@clearfirestudios.com>
2013-10-16 13:29:57 -0500

    Fix window url detection.

--- js/jquery.localDimensions.js
+++ js/jquery.localDimensions.js
@@ -16,7 +16,7 @@
 				windowWidth,
 				windowHeight,
 				devUrl = settings.devUrl,
-				currentUrl = String(window.location);
+				currentUrl = String(window.location.origin);
 
 				//Setup position
 				setPosition = function() {
@@ -76,10 +76,7 @@
 				// Make sure user passed a dev url.
 				// Dimensions won't show if this isn't set
 				checkDevUrl = (function() {
-					if (devUrl !== currentUrl) {
-						appendContainer();
-						$dimensions.html('<p style="margin-bottom: 0;"><strong>You forgot or mistyped your Local Dev URL!</strong><br>Your current url is: <strong>' + currentUrl + '</strong><br>If that\'s your local dev url, set up dimesions like this:<br><code>$("body").dimensions({devUrl: "' + currentUrl + '"});</code></p>');
-					} else {
+					if (devUrl === currentUrl) {
 						appendContainer();
 						showDimensions();
 						setupMoveDimensions();
